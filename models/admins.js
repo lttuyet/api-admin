81836adf7ca56df0a5f39c14adce451c73bac9ed
Anh Từ <44427982+anhtu163@users.noreply.github.com>
2019-12-18 15:05:31 +0700

    Update admins.js

--- models/admins.js
+++ models/admins.js
@@ -4,54 +4,62 @@ const SALT_ROUNDS = 10;
 const bcrypt = require("bcrypt");
 var mongoose = require("mongoose");
 
-module.exports.findAdminByUsername = async (_username) => {
-  return await dbs.production.collection("admins").findOne({ username: _username });
+module.exports.findAdminByUsername = async _username => {
+  return await dbs.production
+    .collection("admins")
+    .findOne({ username: _username,isDeleted:false });
 };
 
-module.exports.findAdminById = async (id) => {
-  return await dbs.production.collection("admins").findById(id);
+module.exports.findAdminById = async id => {
+  return await dbs.production.collection("admins").findOne(id);
 };
 
-module.exports.insertAdmin = async (admin) => {
-  const hash = await bcrypt.hash(admin.password, SALT_ROUNDS);
-  const newAdmin = {
-    name: admin.name,
-    username: admin.username,
-    address: admin.address,
-    phone: admin.phone,
-    email: admin.email,
-    password: hash,
+module.exports.insertAdmin = async admin => {
+  const existAdmin = await dbs.production.collection("admins").findOne({
+    username:admin.username,
     isDeleted: false
-  };
+  });
 
-  return await dbs.production.collection("admins").insertOne(newAdmin);
+  if (!existAdmin) {
+    const hash = await bcrypt.hash(admin.password, SALT_ROUNDS);
+    const newAdmin = {
+      name: admin.name,
+      username: admin.username,
+      address: admin.address,
+      phone: admin.phone,
+      email: admin.email,
+      password: hash,
+      isDeleted: false
+    };
+
+    return await dbs.production.collection("admins").insertOne(newAdmin);
+  } else {
+    return false;
+  }
 };
 
-module.exports.getAllAdmin = async () =>{
-  try{
-    return await dbs.production.collection('admins').find({isDeleted: false}).toArray();
-  }catch(e){
+module.exports.getAllAdmin = async () => {
+  try {
+    return await dbs.production
+      .collection("admins")
+      .find({ isDeleted: false })
+      .toArray();
+  } catch (e) {
     return false;
   }
-}
+};
 
-module.exports.getAllAdmin = async () =>{
-  try{
-    return await dbs.production.collection('admins').find({isDeleted: false}).toArray();
-  }catch(e){
-    return false;
-  }
-}
-
-module.exports.deleteAdmin = async (id) =>{
-  try{
-    return await dbs.production.collection('admins').updateOne({ _id: ObjectId(id) },
-    {
-      $set: {
-        isDeleted: true
+module.exports.deleteAdmin = async id => {
+  try {
+    return await dbs.production.collection("admins").updateOne(
+      { _id: ObjectId(id) },
+      {
+        $set: {
+          isDeleted: true
+        }
       }
-    });
-  }catch(e){
+    );
+  } catch (e) {
     return false;
   }
-}
+};
